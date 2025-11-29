98dea903dfd3f589d4462450acbd48ad5a769dc8
Mateus2411 <mateushenriquedasilva2411@gmail.com>
2025-11-29 15:53:24 -0300

    bah

--- frontend vue/src/App.vue
+++ frontend vue/src/App.vue
@@ -1,8 +1,34 @@
-<script setup></script>
+<script setup>
+import AppHeader from './components/Componentes Globais/AppHeader.vue'
+import AppFooter from './components/Componentes Globais/AppFooter.vue'
+</script>
 <template>
-  <div id="app">
-    <router-view />
+  <div id="layout">
+    <app-header />
+    <main>
+      <router-view />
+    </main>
+    <app-footer />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+html,
+body,
+#app,
+#layout {
+  height: 100%;
+  margin: 0;
+}
+
+#layout {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+main {
+  flex: 1;
+  /* EMPURRA o footer pro fundo */
+}
+</style>
--- frontend vue/src/router/index.js
+++ frontend vue/src/router/index.js
@@ -4,16 +4,21 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: () => import("@/views/Home/HomeView.vue")
+      path: '/',
+      name: 'home',
+      component: () => import('@/views/Home/HomeView.vue'),
     },
     {
-      path: "/cadastrar",
-      name: "cadastrar",
-      component: () => import("@/views/Cadastro & Loguin/CadastroLoguin.vue")
-    }
-  ]
+      path: '/cadastrar',
+      name: 'cadastrar',
+      component: () => import('@/views/Cadastro & Loguin/CadastroLoguin.vue'),
+    },
+    {
+      path: '/ias',
+      name: 'IAs',
+      component: () => import('@/views/IAs/IAs.vue'),
+    },
+  ],
 })
 
 export default router
